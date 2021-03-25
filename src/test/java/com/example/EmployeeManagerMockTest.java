46f64fe2acd70c725aa01c654746dbababad982e
Arif Hossain <arif.hossain@iths.se>
2021-03-25 23:48:45 +0100

    Uppgift 2: Test double with mockito

---
+++ src/test/java/com/example/EmployeeManagerMockTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class EmployeeManagerMockTest {
+    static EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+    static BankService bankService = mock (BankService.class);
+    static EmployeeManager employeeManager = new EmployeeManager (employeeRepository,bankService);
+
+    @Test
+    void makePayment(){
+    when (employeeRepository.findAll ()).thenReturn (List.of (new Employee ("1",30000),
+            new Employee ("2", 35000)));
+    assertEquals (2,employeeManager.payEmployees ());
+    }
+}
