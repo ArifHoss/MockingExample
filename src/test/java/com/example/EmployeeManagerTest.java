433ebc1d102403dedbacf3b9b209f1c48b58b0be
Arif Hossain <arif.hossain@iths.se>
2021-03-25 21:03:04 +0100

    Uppgift 2: Use test double second test(set payment false)

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -3,19 +3,23 @@ package com.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class EmployeeManagerTest {
 
+    static EmployeeRepository employeeRepository = new testEmployRepository();
+    static BankService bankService = new TestBankService();
+    static EmployeeManager employeeManager = new EmployeeManager (employeeRepository,bankService);
+
     @Test
     void makePayment(){
-        EmployeeRepository employeeRepository = new testEmployRepository();
-        BankService bankService = new TestBankService();
-        EmployeeManager employeeManager = new EmployeeManager (employeeRepository,bankService);
-
-        //var actual =  employeeManager.payEmployees ();
         assertEquals(2,employeeManager.payEmployees ());
     }
 
-
+    @Test
+    void setPaymentFalse(){
+        assertFalse(employeeManager.payEmployees ()==0);
+    }
 
 }
